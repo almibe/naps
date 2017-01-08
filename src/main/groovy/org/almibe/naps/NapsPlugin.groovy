23796185582c2be6e10a626ea3b10b637aba7adf
Alex Berry <berry3@indiana.edu>
2017-01-08 04:06:13 -0500

    starting to use copy task to handle copying and processing files

--- src/main/groovy/org/almibe/naps/NapsPlugin.groovy
+++ src/main/groovy/org/almibe/naps/NapsPlugin.groovy
@@ -2,8 +2,14 @@ package org.almibe.naps
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.Copy
 
 class NapsPluginExtension {
+    String contentsIn = "src/naps/contents"
+    String fragmentsIn = "src/naps/fragments"
+    String templatesIn = "src/naps/templates"
+    String siteOut = "naps/site"
+
     String defaultTemplate = ""
     List<TemplateDefinition> templateDefinition = []
 }
@@ -19,11 +25,22 @@ class NapsPlugin implements Plugin<Project> {
 
         project.extensions.create("naps", NapsPluginExtension)
 
-        project.task("naps") {
-            //TODO copy over all resources except .txt files process those
-            //TODO convert them with asciidoctorj and run them through the configured groovy template
-            //TODO have a default template and also have a way of defining template matches with a matcher function
-            //TODO if none of the registered matcher functions pass then use the default
+        project.task("naps", type:Copy) {
+            from "${project.naps.contentsIn}"
+            into "${project.buildDir}/${project.naps.siteOut}"
+
+            eachFile {
+                if (it.name.endsWith('.json')) { //exclude .json files if there is a .txt file with it's same name
+                    if (it.file.path.resolveSibling(it.name.trim(5) + ".txt") != null) {
+                        it.exclude()
+                    }
+                }
+                if (it.name.endsWith('.txt')) {
+                    //TODO convert them with asciidoctorj and run them through the configured groovy template
+                    //TODO have a default template and also have a way of defining template matches with a matcher function
+                    //TODO if none of the registered matcher functions pass then use the default
+                }
+            }
         }
     }
 }
